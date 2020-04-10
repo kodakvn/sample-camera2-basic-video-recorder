340d8a8d73b8786f11a3dc42046bbb9d29d34a1b
KODAK <linhtt.it@gmail.com>
2020-04-10 10:45:22 +0700

    init project

---
+++ app/src/main/java/com/kodak/basicvideorecorder/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kodak.basicvideorecorder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
